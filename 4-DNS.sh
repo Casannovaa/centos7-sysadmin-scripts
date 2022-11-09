fda780bc59f8c619aad1fca77aa1b1598883e376
Casanova <guillemteixido2@gmail.com>
2022-11-09 21:24:54 +0100

    Update 4-DNS.sh

--- 4-DNS.sh
+++ 4-DNS.sh
@@ -5,7 +5,7 @@
 # Daemons used: named
 
 # Hostname Domain Change
-read -p "Declare new hostname (With Domain included [terronn.whatever.com]) > " hst
+read -p "Declare new hostname (With Domain included [whatever.whatever.whatever]) > " hst
 echo "$hst" > /etc/hostname
 
 # Installing DNS tools (bind)
@@ -26,17 +26,6 @@ ddom=$(cat /etc/dhcp/dhcpd.conf | grep "option domain-name " | awk -F \" '{print
 
 
 echo '
-//
-// named.conf
-//
-// Provided by Red Hat bind package to configure the ISC BIND named(8) DNS
-// server as a caching only nameserver (as a localhost DNS resolver only).
-//
-// See /usr/share/doc/bind*/sample/ for example named configuration files.
-//
-// See the BIND Administrator`s Reference Manual (ARM) for details about the
-// configuration located in /usr/share/doc/bind-{version}/Bv9ARM.html
-
 options {
 	listen-on port 53 { 127.0.0.1; '$servip'; };
 	# listen-on-v6 port 53 { ::1; };
@@ -47,27 +36,13 @@ options {
 	recursing-file  "/var/named/data/named.recursing";
 	secroots-file   "/var/named/data/named.secroots";
 	allow-query     { any; };
-
-	/* 
-	 - If you are building an AUTHORITATIVE DNS server, do NOT enable recursion.
-	 - If you are building a RECURSIVE (caching) DNS server, you need to enable 
-	   recursion. 
-	 - If your recursive DNS server has a public IP address, you MUST enable access 
-	   control to limit queries to your legitimate users. Failing to do so will
-	   cause your server to become part of large scale DNS amplification 
-	   attacks. Implementing BCP38 within your network would greatly
-	   reduce such attack surface 
-	*/
 	recursion yes;
-
 	dnssec-enable yes;
 	dnssec-validation yes;
 
 	/* Path to ISC DLV key */
 	bindkeys-file "/etc/named.root.key";
-
 	managed-keys-directory "/var/named/dynamic";
-
 	pid-file "/run/named/named.pid";
 	session-keyfile "/run/named/session.key";
 };
@@ -89,15 +64,13 @@ zone "'"$ddom"'" IN {
     file "named.'"$ddom"'";
 };
 
-
 include "/etc/named.rfc1912.zones";
 include "/etc/named.root.key";' > /etc/named.conf
 
 echo "Domain used for DNS server configuration --> $ddom"
-echo "If don't want that, change manually to /etc/named.conf"
-sleep 1
+echo "If don't want that, change manually in /etc/named.conf"
 
-read -p "Bind named process option [Default: whatever | Recommended: -4] --> " bind
+read -p "Bind named process option [Recommended: -4] --> " bind
 echo '
 # BIND named process options
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -129,8 +102,13 @@ echo "
                             604800  ; TTL negative cache
 );
 
-; Servidors de noms
+; NameServers
 @               IN      NS      $hst.
 
-; Registres
+; Registers
 $name       IN      A       $servip" > /var/named/named."$ddom"
+
+echo "DNS should be configured successfully"
+systemctl start named
+systemctl enable named
+echo "You might need to reboot the PC"
